3b0843f93a12cdb02073d1681b15e4cb7ab8f193
sunfeng <sunfeng@corp.netease.com>
2024-05-30 22:12:16 +0800

    add

--- web/main.go
+++ web/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/spf13/viper"
 )
 
@@ -19,10 +21,14 @@ type Config struct {
 
 var Conf = new(Config)
 
-// //GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
 const (
 	code2sessionURL = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
 )
+
+type MyCustomClaims struct {
+	OpenID string `json:"openid"`
+	jwt.RegisteredClaims
+}
 
 func main() {
 	viperConf := viper.New()
@@ -62,6 +68,18 @@ func main() {
 }
 
 // /http://localhost:8375/code2Session?appid=wxb00370e58ccf0603&code=1111
+
+/*
+GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+{
+	"openid":"xxxxxx",
+	"session_key":"xxxxx",
+	"unionid":"xxxxx",
+	"errcode":0,
+	"errmsg":"xxxxx"
+}
+*/
+
 func code2Session(c *gin.Context) {
 	appid := c.Query("appid")
 	code := c.Query("code")
@@ -89,4 +107,48 @@ func code2Session(c *gin.Context) {
 		"success": true,
 		"openId":  result["openid"],
 	})
+}
+
+var mySigningKey = []byte("AllYourBase")
+
+// 生成token
+func SetToken(openid string) (string, error) {
+	claims := MyCustomClaims{
+		OpenID: openid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Hour)), //有效时间
+			IssuedAt:  jwt.NewNumericDate(time.Now()),                    //签发时间
+			NotBefore: jwt.NewNumericDate(time.Now()),                    //生效时间
+			Issuer:    "test",                                            //签发人
+			Subject:   "somebody",                                        //主题
+			ID:        "1",                                               //JWT ID用于标识该JWT
+			Audience:  []string{"somebody_else"},                         //用户
+		},
+	}
+
+	//使用指定的加密方式和声明类型创建新令牌
+	tokenStruct := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	//获得完整的、签名的令牌
+	token, err := tokenStruct.SignedString(mySigningKey)
+	return token, err
+}
+
+// 验证token
+func CheckToken(token string) (*MyCustomClaims, error) {
+	//解析、验证并返回token
+	token, err := jwt.ParseWithClaims(token, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return mySigningKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*MyCustomClaims); ok {
+		fmt.Printf("%v %v\n", claims.OpenID, claims.RegisteredClaims)
+		return claims, nil
+	} else {
+		return nil, err
+	}
 }
