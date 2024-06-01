4c664a29c1260a8565b005ad9523091b2e1b1003
sunfeng <sunfeng@corp.netease.com>
2024-06-01 16:35:13 +0800

    添加注释

--- web/router/routers.go
+++ web/router/routers.go
@@ -36,6 +36,7 @@ func Router() *gin.Engine {
 	user := r.Group("user")
 	{
 		userController := controllers.UserController{}
+		// /user/info
 		user.GET("/info", userController.GetUserInfo)
 		user.POST("/list", userController.GetList)
 		user.PUT("/add", userController.AddUser)
@@ -93,3 +94,77 @@ func code2Session(c *gin.Context) {
 		"openId":  result["openid"],
 	})
 }
+
+/*
+HTTP上传参数3个部分：Header 、URL、 HTTP Body
+Header：键值对集合 Content-Type Accept
+
+URL：请求路径， http://localhost:8080/user/add/1
+	参数获取：ctx.Param("id")
+	//http://localhost:8080/user/100
+	engine.GET("/user/:id", func(ctx *gin.Context) {
+    	id := ctx.Param("id")
+	}
+
+
+HTTP Body：请求体所携带的参数， Content-Type：application/json时 body是一个json串
+    获取URL Query中的参数
+	  //http://localhost:8080/user/list?name=test&gender=xxxx'
+	  engine.GET("/user/list", func(ctx *gin.Context) {
+		//获取单个值
+		name := ctx.Query("name")
+		//带默认值
+		gender := ctx.DefaultQuery("gender", "男")
+		//数组
+		habits := ctx.QueryArray("habits")
+		//map
+		works := ctx.QueryMap("works")
+		fmt.Printf("%s, %s, %s, %s\n", name, gender, habits, works)
+  })
+
+  HTTP Body中的参数
+    engine.POST("/user/add", func(ctx *gin.Context) {
+		//获取单个值
+		name := ctx.PostForm("name")
+		//带默认值
+		gender := ctx.DefaultPostForm("gender", "男")
+		//数组
+		habits := ctx.PostFormArray("habits")
+		//map
+		works := ctx.PostFormMap("works")
+		fmt.Printf("%s,%s,%s,%s\n", name, gender, habits, works)
+  })
+
+//绑定请求参数 BindUri()或者ShouldBindUri()
+type User struct {
+  Name  string `uri:"name"`
+  Email string `uri:"email"`
+}
+engine.GET("/user/list/:id/:name", func(ctx *gin.Context) {
+	var u User
+	if err := ctx.BindUri(&u);err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+	fmt.Fprintf(ctx.Writer, "你输入的用户名为：%s,邮箱为：%s\n", u.Name, u.Email)
+})
+
+//绑定URL Query参数
+    engine.GET("/user/list", func(ctx *gin.Context) {
+        var u User
+        if err := ctx.BindQuery(&u);err != nil {
+            ctx.JSON(http.StatusBadRequest, err)
+            return
+        }
+        fmt.Fprintf(ctx.Writer, "你输入的用户名为：%s,邮箱为：%s\n", u.Name, u.Email)
+    })
+//绑定HTTP Body参数,POST请求时才会进行绑定  Bind BindJSON()
+    engine.POST("/user/add", func(ctx *gin.Context) {
+        var u User
+        if err := ctx.Bind(&u); err != nil {
+            ctx.JSON(http.StatusBadRequest, err.Error())
+            return
+        }
+        fmt.Fprintf(ctx.Writer, "你输入的用户名为：%s,邮箱为：%s\n", u.Name, u.Email)
+    })
+*/
